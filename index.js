21ac6e3785d32a050fc0f3da279e72bf2e970eb3
Julia Domingues <142114663+julooch@users.noreply.github.com>
2024-08-08 12:30:54 -0300

    feat: install nodemon

--- index.js
+++ index.js
@@ -3,4 +3,8 @@
 // chamar um módulo
 const express = require('express')
 
+// o express irá criar o servidor e disponibilizar as rotas (endpoints)
 const app = express()
+
+// função para rodar o servidor numa porta 3000
+app.listen(3000, () => console.log('Servidor rodando na porta 3000')) 
