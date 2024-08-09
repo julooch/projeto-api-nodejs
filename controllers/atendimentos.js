c4f15e3157962816d797c442f5218aed3b8e073c
Julia Domingues <142114663+julooch@users.noreply.github.com>
2024-08-09 18:15:01 -0300

    feat: atualizaçao controllers

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,5 +1,15 @@
 // o express irá criar o servidor e disponibilizar as rotas (endpoints)
 
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send("Você está na rota de atendimentos"))
+
+
+    app.post('/atendimentos', (req, res) => {
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento)
+        res.send('Post atendimento')
+    })
 }
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const customExpress = require('./config/customExpress')
 const conexao = require('./infra/conexao')
+const Tabelas = require('./infra/tabelas')
 
 conexao.connect(erro => {
     if(erro){
         console.log(erro)
     }else {
+        console.log('conectado com sucesso')
+        Tabelas.init(conexao)
         const app = customExpress()
 
         // função para rodar o servidor numa porta 3000
